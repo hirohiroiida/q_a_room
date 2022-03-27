1cc4c8118ddbc3faf7f65efb501e2ea2fa31f3ab
Hピエトロ <awpak1208h@gmail.com>
2022-03-28 07:10:57 +0900

    add userとadmin/user周りを実装、他色々

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :new, :create, :show,  :edit, :destroy] 
+  namespace :admin do
+    resources :users, only: [:index, :new, :create, :show,  :edit, :destroy]
+  end
 end

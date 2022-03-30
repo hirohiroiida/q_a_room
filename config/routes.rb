b4fde714877875302f36934d29e864b7dcd1754c
Hピエトロ <awpak1208h@gmail.com>
2022-03-30 16:55:08 +0900

    add ログイン機能を導入 sessionsコントローラ等追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :users, only: [:index, :new, :create, :show,  :edit, :destroy] 
   namespace :admin do
     resources :users, only: [:index, :new, :create, :show,  :edit, :destroy]
+    get '/login', to: 'sessions#new'
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
   end
 end

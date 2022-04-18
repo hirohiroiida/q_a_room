477781371158dc7ad076a5dccb07ea131db844ff
Hピエトロ <awpak1208h@gmail.com>
2022-04-18 22:37:26 +0900

    add 画像未設定の場合の差し替え画像実装、その他微調整

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,7 +43,7 @@ class QuestionsController < ApplicationController
   
 
   def show
-    @question = current_user.questions.find(params[:id])
+    @question = Question.find(params[:id])
     @answer = Answer.new
   end
 

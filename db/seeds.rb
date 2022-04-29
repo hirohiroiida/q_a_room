98f9012a314fb1d4142f2a067f07780d2684f246
Hピエトロ <awpak1208h@gmail.com>
2022-04-29 17:36:30 +0900

    add Fakerを使ってseedから初期データ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(
-  username:  '管理者',
+user = User.create!(
+  name:  '管理者',
   email: 'admin@gmail.com',
   password:  '123456',
   password_confirmation: '123456',
   admin: true
 )
+
+20.times do
+  user = User.create!(
+    name: Faker::JapaneseMedia::StudioGhibli.unique.character,
+    email: Faker::Internet.unique.email,
+    password: '123456',
+    password_confirmation: '123456',
+  )
+  puts "\"#{user.name}\" has created!"
+end
+
+User.all.each do |user|
+  user.questions.create!(
+    title: 'タイトル',
+    body: 'テキストテキストテキストテキスト'
+  )
+end
